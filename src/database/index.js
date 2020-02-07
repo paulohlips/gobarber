d53c4b6fdc184a908273e1f0aef47367a76ce6b3
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2020-02-07 14:32:20 -0300

    feat: userController & User model

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+//Arquivo responsável por iniciar conexão com o banco de dados e carregar as models
+import Sequelize from "sequelize";
+
+import User from "../app/models/User";
+
+import databaseConfig from "../config/database";
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
