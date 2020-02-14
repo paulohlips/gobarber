e5620e3125a99ae4e7f9eec7f7f2f8fce39e6b35
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2020-02-14 12:55:16 -0300

    feat: listing provider schedule

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,6 +7,7 @@ import File from "../models/File";
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -14,6 +15,8 @@ class AppointmentController {
       },
       order: ["date"],
       attributes: ["id", "date"],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
