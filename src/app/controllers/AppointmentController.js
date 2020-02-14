ea7c2d01562b6a8f95e61693ed6abbcbec0e4869
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2020-02-14 15:13:11 -0300

    feat: notifications with mongodb

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore } from "date-fns";
+import { startOfHour, parseISO, isBefore, format, subHours } from "date-fns";
+import pt from "date-fns/locale/pt";
 
 import User from "../models/User";
 import Appointment from "./../models/Appointment";
 import File from "../models/File";
+
+import Notification from "../schemas/Notification";
 
 class AppointmentController {
   async index(req, res) {
@@ -95,6 +98,43 @@ class AppointmentController {
       provider_id,
       date
     });
+
+    /*
+     *  Notify appointment user
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
+      user: provider_id
+    });
+
+    return res.json({ appointment });
+  }
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment."
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: "You can only cancel appointments 2 hours in advance"
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json({ appointment });
   }
